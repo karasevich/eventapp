78fa2feb7b6a2725166e819e86c0f46e50e10c73
karasevich <karasevich@gmail.com>
2024-05-20 12:13:06 +0300

    hosting changes5

--- server/server.js
+++ server/server.js
@@ -1,23 +1,23 @@
 const dotenv = require('dotenv')
 const express = require('express')
-const cors = require('cors')
+// const cors = require('cors')
 const bodyParser = require('body-parser')
 const path = require('path')
 const mysql = require('mysql2')
 
 dotenv.config()
 
-const allowedOrigins = ['http://localhost:3000', 'https://eventapp-nodejs-mysql-35971d322429.herokuapp.com/'];
+// const allowedOrigins = ['http://localhost:3000', 'https://eventapp-nodejs-mysql-35971d322429.herokuapp.com/'];
 
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  }
-}));
+// app.use(cors({
+//   origin: function (origin, callback) {
+//     if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   }
+// }));
 
 const app = express()
 const PORT = process.env.PORT || 5000
